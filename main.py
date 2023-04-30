66b79a434bca3543dba9eeda57a8418e7b0f303e
ASafin <ayah.safeen@gmail.com>
2023-04-30 11:20:04 +0300

    fixed function bla bla bla

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_2(msg):
     print(msg)
 
-
+def print_3232():
+    return "hu"
 
 if __name__ == '__main__':
     print_2("Hello Sahar")
@@ -9,4 +10,4 @@ if __name__ == '__main__':
     print_2("hiii")
 
     print_2("hi aya")
-    print_2("kuhdshgfdjyh")
+    print_2("kuhd
