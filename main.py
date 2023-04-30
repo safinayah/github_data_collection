516009fef6ad1d0658a83738ba210afe74a3d1ea
ASafin <ayah.safeen@gmail.com>
2023-04-30 11:21:24 +0300

    fixed error

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def print_2(msg):
     print(msg)
 
-def print_3232():
-    return "hu"
+
 
 if __name__ == '__main__':
     print_2("Hello Sahar")
@@ -10,4 +9,4 @@ if __name__ == '__main__':
     print_2("hiii")
 
     print_2("hi aya")
-    print_2("kuhd
+    print_2("kuhdshgfdjyh")
